4f64f0cd5a7341de51ca4781f0246790ee0fa571
mayuriyam <mayuriya21@gmail.com>
2017-06-30 18:01:35 +0530

    Create chara.java

---
+++ chara.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class chara
+{
+    public static void main(String ar[])
+    {
+        Scanner sc=new Scanner (System.in);
+        String a=sc.nextLine();
+        int b=a.length();
+        System.out.println(b);
+       
+    }
+}
